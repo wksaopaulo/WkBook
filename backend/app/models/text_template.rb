3cb8bc67d34229cf2253939dd1bbd04b4313871e
Lucas Dupin <lucas.dupin@wk.com>
2012-01-19 18:16:38 -0200

    validation

--- backend/app/models/text_template.rb
+++ backend/app/models/text_template.rb
@@ -2,8 +2,6 @@ class TextTemplate < ActiveRecord::Base
   has_many :users
 
   mount_uploader :picture, TextTemplatePictureUploader
-  validates_presence_of :min_text
-  validates_presence_of :max_text
   validates_presence_of :picture
   validates_presence_of :text_id
 
--- backend/db/migrate/20120119184048_add_text_id_to_text_templates.rb
+++ backend/db/migrate/20120119184048_add_text_id_to_text_templates.rb
@@ -1,7 +1,7 @@
 class AddTextIdToTextTemplates < ActiveRecord::Migration
   def change
     add_column :text_templates, :text_id, :integer
-    remove_column :text_templates, :min_text, :integer
-    remove_column :text_templates, :max_text, :integer
+    remove_column :text_templates, :min_text
+    remove_column :text_templates, :max_text
   end
 end
