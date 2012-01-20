398d69764649b13f26c916510e317b06799b1203
Lucas Dupin <lucas.dupin@wk.com>
2012-01-20 11:44:21 -0200

    separating by number of characters again

--- backend/app/controllers/book_creator_controller.rb
+++ backend/app/controllers/book_creator_controller.rb
@@ -41,8 +41,8 @@ class BookCreatorController < ApplicationController
     text_size += current_user.template_text.size rescue 0
     text_size /= 10 #word count approximate
     puts "text_size is #{text_size}"
-    #@templates = TextTemplate.find :all, :conditions => ["? >= min_text and ? <= max_text", text_size, text_size]
-    @templates = TextTemplate.all # if @templates.empty?
+    @templates = TextTemplate.find :all, :conditions => ["? >= min_text and ? <= max_text", text_size, text_size]
+    @templates = TextTemplate.all if @templates.empty?
   end
 
   def save_image
